80c6f19706b2f97c51a0c8f9570b1b93c5a06ef9
Gabriel Lopes Ferreira Ramos <gabrielramos149@gmail.com>
2018-07-24 14:34:18 -0300

    Update index.js

--- js/hipercode/index.js
+++ js/hipercode/index.js
@@ -1,5 +1,5 @@
 document.querySelector("head").innerHTML = "<title>NoisyTech - Official Website</title>"+document.querySelector("head").innerHTML;
-HC.include("./html/head.html", function(data){
+HC.include("./html/header.html", function(data){
 	document.querySelector("body").innerHTML = document.querySelector("body").innerHTML+data;
 });
 HC.include("./html/indexContent.html", function(data){
