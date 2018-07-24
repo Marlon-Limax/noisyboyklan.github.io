a56f333873fdf691617b7f1eca9cd81838d51db7
Gabriel Lopes Ferreira Ramos <gabrielramos149@gmail.com>
2018-07-24 14:53:25 -0300

    Update index.js

--- js/hipercode/index.js
+++ js/hipercode/index.js
@@ -4,19 +4,19 @@ function generateContent(n = 0){
 		case 0:
 			HC.include("./html/header.html", function(data){
 				document.querySelector("body").innerHTML = document.querySelector("body").innerHTML+data;
-				generateContent(n++);
+				generateContent(n+1);
 			});
 			break;
 		case 1:
 			HC.include("./html/indexContent.html", function(data){
 				document.querySelector("body").innerHTML = document.querySelector("body").innerHTML+data;
-				generateContent(n++);
+				generateContent(n+1);
 			});
 			break;
 		case 2:
 			HC.include("./html/footer.html", function(data){
 				document.querySelector("body").innerHTML = document.querySelector("body").innerHTML+data;
-				generateContent(n++);
+				generateContent(n+1);
 			});
 			break;
 		default:
